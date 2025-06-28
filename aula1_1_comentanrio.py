e018b2d5409ec803f56f7902d4505d0f3f6b1ad3
klebermsilva <klebermsilva30@gmail.com>
2025-06-28 19:17:18 -0300

    Create aula1_1_comentanrio.py

---
+++ aula1_1_comentanrio.py
@@ -0,0 +1,27 @@
+# Permite escrever um comentário - aqui o asterisco comenta uma linha 
+
+# -----------------------------------------------------------------------
+
+# Aqui as três aspas duplas ou DocString  permite escrever o que quiser e o interpretar 
+# Pula ele e interpreta o proximo código valido 
+
+"""  
+DocString
+E escrever o que eu
+quiser
+
+
+asdfasdfd
+"""
+# --------------------------------------------------------------------------
+  # O mesmo ocorre com as aspas simples 
+
+
+''' Usar para escrever suas notas '''
+
+# -------------------------------------------------------------------------------
+  # Abaixo esta uma outra manira de utizar o hashtag na frente do codigo
+
+print(123)  # Na frente
+# Abaixo
+print(456)
