5fa27c505e9c652f92838afeef127b7dac71448d
klebermsilva <klebermsilva30@gmail.com>
2025-06-28 19:40:09 -0300

    Create aula3_string.py

---
+++ aula3_string.py
@@ -0,0 +1,26 @@
+# Aqui está sendo apresentado uma breve explicação de como é o Python 
+# E mostra como é utilizado o conceito de string ou str 
+
+
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('kleber marques')
+print(1, 'Kleber "Silva"')
+
+# Aspas duplas
+print("kleber marques")
+print(2, "Kleber 'Silva'")
+
+# Escape
+print("Kleber \"Thalyta\"")
+
+# r
+print(r"Thalyta \"Kleber\"")
