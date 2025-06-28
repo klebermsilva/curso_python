62ba47b5306039ef92aab031247da428a048845a
klebermsilva <klebermsilva30@gmail.com>
2025-06-28 19:31:14 -0300

    Create aula2_funcoes.py

---
+++ aula2_funcoes.py
@@ -0,0 +1,19 @@
+
+# \r\n -> CRLF - Quebra de linha do windows
+# \n -> LF - Quebra de linha de outros sistemas
+
+
+
+
+''' 
+A função print (Serve para exibir algo na tela)
+O sep= (Serve para separar utizando caracter ou espaço)
+Outro argumento end= (Serve para quebrar linha ou outra coisa)
+Tudo em aspas simples ou duplas
+''''
+
+
+
+print(12, 34, 1011, sep="", end='#')
+print(56, 78, sep='-', end='\n')
+print(9, 10, sep='-', end='\n')
