4f7947e8d817d8ad9c389786110e24d83735b5e9
klebermsilva <klebermsilva30@gmail.com>
2025-07-01 18:22:49 -0300

    Create aula8_1_concatenacao_op_arit.py

---
+++ aula8_1_concatenacao_op_arit.py
@@ -0,0 +1,11 @@
+# Nesta aula é apresentado maneiras de concatenação 
+
+#--------------------------------------------------------------------------
+
+concatenacao = 'Kleber' + ' ' + 'Silva'
+print(concatenacao)
+
+vinte_vezes = '&' * 20
+nome = 6 * 'Kleber'
+print(vinte_vezes)
+print(nome)
