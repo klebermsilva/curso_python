ea8dc6023863cabeda99e273f4b15becfc71c86b
klebermsilva <klebermsilva30@gmail.com>
2025-07-02 15:43:33 -0300

    Condição

---
+++ aula13_explicacao_da_condicao.py
@@ -0,0 +1,14 @@
+# if / elif / else
+#--------------------
+# Se / Se não se / Se não
+
+condicao = input('Informe a condição: ')
+
+if condicao == True:
+    print('Condição verdadeira') 
+elif condicao == False:
+    print('Condição falsa')
+elif condicao == '"A" ou "a"':
+    print('Condição não condizente')
+else:
+    print('Tente novamente')
