b6d5439fd6d22088a09e018108ce18a5a0f8c897
klebermsilva <klebermsilva30@gmail.com>
2025-07-01 22:55:56 -0300

    formatação metodo

---
+++ 10_1_format_strng_metodos.py
@@ -0,0 +1,12 @@
+
+# Esta aula ensina a formatar com metodo
+
+a = 'AAAAA'
+b = 'BBBBBB'
+c = 1.1
+string = 'b={nome2} a={nome1} a={nome1} c={nome3:.2f}'
+formato = string.format(
+    nome1=a, nome2=b, nome3=c
+)
+
+print(formato)
