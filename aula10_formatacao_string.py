96f8b34ddfc5a32d7010d4c34f5eb5905d3fe9b8
klebermsilva <klebermsilva30@gmail.com>
2025-07-01 22:52:24 -0300

    formatação_string

---
+++ aula10_formatacao_string.py
@@ -0,0 +1,22 @@
+
+# --------------------------------------------------------
+    # ----------- Aqui é falado sobre a formação ------------
+
+nome = 'Thalyta Cristina'
+altura = 1.65
+peso = 115
+imc = peso / altura ** 2
+
+# "f-strings"
+
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_2 = f'pesa {peso} quilos e seu imc é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
+
+# Thalyta Cristina tem 1.65 de altura,
+# pesa 115 quilos e seu IMC é
+# 42.24
