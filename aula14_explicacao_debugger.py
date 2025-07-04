569b3c08479f473ebf8004b340cdf23583a68918
klebermsilva <klebermsilva30@gmail.com>
2025-07-04 18:45:56 -0300

    explicação do debbuguer

---
+++ aula14_explicacao_debugger.py
@@ -0,0 +1,25 @@
+# if / elif / else
+#--------------------
+# Se / Se não se / Se não
+
+condicao1 = False
+condicao2 = False
+condicao3 = True
+
+
+if condicao1:
+    print('Condição verdadeira') 
+elif condicao2:
+    print('Condição falsa')
+elif condicao3:
+    print('Condição não condizente')
+else:
+    print('Tente novamente')
+
+#-------------------------------------------
+
+if 10 == 10:
+    print('Outro if')
+
+
+print('Fora')
