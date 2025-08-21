b96f3d65c576ab60e8515c33eecee9eee5892bca
klebermsilva <klebermsilva30@gmail.com>
2025-08-21 17:46:47 -0300

    Exercicio do Impar, Par

---
+++ exercicios_5.py
@@ -0,0 +1,16 @@
+# Nesta  parte do programa será solicitado ao usuário um número inteiro 
+
+inteiro = input("Informe um número inteiro? ")
+
+
+# Verificação se o valor émpar ou Par 
+
+numero = int(inteiro)
+
+if numero % 2 == 0:
+   print(f"O valor informado {numero} e ele é Par")
+else:
+   print(f"O valor informado {numero} e ele é Impar")
+
+
+
